754846a22a13563c14263733a7537c643e4e1cd3
Abdallah Mohamed <abdallah.mohamed@kyocera-avx.com>
2023-12-02 14:47:40 -0500

    Fix URLs in website templates

--- website/urls.py
+++ website/urls.py
@@ -9,11 +9,10 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('login_endpoint/', views.login_endpoint, name='login_endpoint'),
     path('logout/', views.logout_user, name='logout'),
-    path('register/', views.register_user, name='register'),
+    path('register/', views.register_user, name='register_user'),
     path('record/<int:pk>', views.customer_record, name='record'),
     path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
     path('add_record/', views.add_record, name='add_record'),
     path('update_record/<int:pk>', views.update_record, name='update_record'),
     path('profile/edit/', views.profile_edit, name='profile_edit'),
-
 ]
