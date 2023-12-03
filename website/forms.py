08a886527b791afa96f6df84aade01bbfbd5ad21
Abdallah Mohamed <abdal_cascad@hotmail.com>
2023-12-02 19:30:25 -0500

    Refactor AddRecordForm in website/forms.py

--- website/forms.py
+++ website/forms.py
@@ -50,51 +50,16 @@ class SignUpForm(UserCreationForm):
         self.fields['password2'].label = ''
 
 class AddRecordForm(forms.ModelForm):
-    # Override the default init method to add a placeholder attribute to each field
-    first_name = forms.CharField(
-        required=True,
-        widget=forms.widgets.TextInput(attrs={"placeholder": "First Name", "class": "block w-full px-4 py-2 mt-1 border rounded-md"}),
-        label=""
-    )
-    last_name = forms.CharField(
-        required=True,
-		widget=forms.widgets.TextInput(attrs={"placeholder": "Last Name", "class": "block w-full px-4 py-2 mt-1 border rounded-md"}),
-		label=""
-	)
-    email = forms.EmailField(
-		required=True,
-		widget=forms.widgets.EmailInput(attrs={"placeholder": "Email Address", "class": "block w-full px-4 py-2 mt-1 border rounded-md"}),
-		label=""
-	)
-    
-# def profile_edit(request):
-#     if request.method == 'POST':
-#         if 'profile_form' in request.POST:
-#             profile_form = ProfileForm(request.POST, instance=request.user)
-#             if profile_form.is_valid():
-#                 profile_form.save()
-#                 # Redirect or indicate success
-#         elif 'password_form' in request.POST:
-#             password_form = PasswordChangeForm(request.user, request.POST)
-#             if password_form.is_valid():
-#                 password_form.save()
-#                 # Redirect or indicate success
-#         elif 'photo_form' in request.POST:
-#             photo_form = ProfilePhotoForm(request.POST, request.FILES, instance=request.user.profile)
-#             if photo_form.is_valid():
-#                 photo_form.save()
-#                 # Redirect or indicate success
-#     else:
-#         profile_form = ProfileForm(instance=request.user)
-#         password_form = PasswordChangeForm(request.user)
-#         photo_form = ProfilePhotoForm(instance=request.user.profile)
-
-#     return render(request, 'profile_edit.html', {
-#         'profile_form': profile_form,
-#         'password_form': password_form,
-#         'photo_form': photo_form
-#     })
-
     class Meta:
         model = Record
-        exclude = ("user",)
+        fields = ['first_name', 'last_name', 'email', 'phone', 'address', 'city', 'state', 'zipcode']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'block w-full px-4 py-2 mt-1 border rounded-md', 'placeholder': 'First Name'}),
+            'last_name': forms.TextInput(attrs={'class': 'block w-full px-4 py-2 mt-1 border rounded-md', 'placeholder': 'Last Name'}),
+            'email': forms.EmailInput(attrs={'class': 'block w-full px-4 py-2 mt-1 border rounded-md', 'placeholder': 'Email'}),
+            'phone': forms.TextInput(attrs={'class': 'block w-full px-4 py-2 mt-1 border rounded-md', 'placeholder': 'Phone'}),
+            'address': forms.TextInput(attrs={'class': 'block w-full px-4 py-2 mt-1 border rounded-md', 'placeholder': 'Address'}),
+            'city': forms.TextInput(attrs={'class': 'block w-full px-4 py-2 mt-1 border rounded-md', 'placeholder': 'City'}),
+            'state': forms.TextInput(attrs={'class': 'block w-full px-4 py-2 mt-1 border rounded-md', 'placeholder': 'State'}),
+            'zipcode': forms.TextInput(attrs={'class': 'block w-full px-4 py-2 mt-1 border rounded-md', 'placeholder': 'Zip Code'}),
+        }
